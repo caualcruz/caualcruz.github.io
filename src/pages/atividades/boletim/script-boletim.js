8170e101ca25176a0dae7c4aeab8cb2635563337
Caua Cruz <caualucas34492@gmail.com>
2023-10-17 22:12:56 -0300

    SEGUNDA PARTE BOLETIM

--- src/pages/atividades/boletim/script-boletim.js
+++ src/pages/atividades/boletim/script-boletim.js
@@ -3,13 +3,25 @@ let materias = [];
 let user = { nome: "", instituicao: "", curso: "" };
 
 
+function userSave(){
+
+  if (!verifVazio("card1")) {
+  
+  user.nome = userNome.value;
+  user.instituicao = nomeInstituicao.value;
+  user.curso = nomeCurso.value
+  document.querySelector("#card1").style.display = "none"
+  document.querySelector("#card2").style.display = "flex"
+}
+}
+
 function save() {
+  let resultados = `<b>BOLETIM DE: ${user.nome.toLocaleUpperCase()}</b><br><b>INSTITUIÇÃO/ESCOLA: ${user.instituicao.toLocaleUpperCase()} </b><b>BOLETIM DE: ${user.nome.toLocaleUpperCase()}</b><br><b>CURSO: ${user.curso.toLocaleUpperCase()} </b>`;
   if (!verifMat()) {
     let inputNotas = [];
     let totalNotas = 0;
-    user.nome = userNome.value;
-    user.instituicao = nomeInstituicao.value;
-    let resultados = `<b>BOLETIM DE: ${user.nome.toLocaleUpperCase()}</b><br><b>INSTITUIÇÃO/ESCOLA: ${user.instituicao.toLocaleUpperCase()} </b>`;
+    
+    
 
     document.querySelectorAll("div.input-group input").forEach((e) => {
       inputNotas.push(parseFloat(e.value));
@@ -19,46 +31,52 @@ function save() {
     if (!validNotas()) {
       materias.push({
         id: materia.id++,
-        nome: select.value.toLocaleUpperCase(),
+        nome: select.value,
         notas: inputNotas,
         total: totalNotas,
         info: `<hr>
-                    RESULTADO DE: <b>${select.value.toLocaleUpperCase()}</b> 
-                    <ul class="list-group">
-                        <li class="list-group-item"><b>1º Bimestre:</b> ${
+                 
+                    <tr>
+
+                    <th>RESULTADO DE: <b>${select.value.toLocaleUpperCase()}</b></th> <br>
+                    
+                   
+                        <td><b>1º Bimestre:</b>${
                           inputNotas[0]
-                        }</li>
-                        <li class="list-group-item"><b>2º Bimestre:</b> ${
+                        }</td><br>
+                        <td><b>2º Bimestre:</b>${
                           inputNotas[1]
-                        }</li>
-                        <li class="list-group-item"><b>3º Bimestre:</b> ${
+                        }</td><br>
+                        <td><b>3º Bimestre:</b>${
                           inputNotas[2]
-                        }</li>
-                        <li class="list-group-item"><b>4º Bimestre:</b> ${
+                        }</td><br>
+                        <td><b>4º Bimestre:</b>${
                           inputNotas[3]
-                        }</li>
-                        <li class="list-group-item">TOTAL FINAL: <b>${totalNotas}</b></li>
-                        <li class="list-group-item">STATUS: <b>${isAprovadoOuReprovado(
-                          totalNotas
-                        )}</b></li>
-                    </ul>
+                        }</td><br>
+                    </tr> 
+                        <td>TOTAL FINAL: <b>${totalNotas}</b></td>
+                        <td><br>
+                          STATUS: <b>${isAprovadoOuReprovado(
+                            totalNotas
+                          )}</b>
+                        </td>
                     `,
       });
     }
 
     function validNotas() {
       for (let i = 0; i < inputNotas.length; i++) {
-        if (i == 0 && inputNotas[0] > 20) {
-          alert("O valor máximo do primeiro bimestre é de 20 pontos");
+        if (i == 0 && inputNotas[0] > 20 || inputNotas[0] < 0) {
+          alert("O valor máximo do primeiro bimestre é de 20 pontos e o mínimo 0");
           return true;
-        } else if (i == 1 && inputNotas[1] > 25) {
-          alert("O valor máximo do segundo bimestre é de 25 pontos");
+        } else if (i == 0 && inputNotas[1] > 25 || inputNotas[1] < 0) {
+          alert("O valor máximo do segundo bimestre é de 25 pontos e o mínimo 0");
           return true;
-        } else if (i == 2 && inputNotas[2] > 25) {
-          alert("O valor máximo do terceiro bimestre é de 25 pontos");
+        } else if (i == 0 && inputNotas[2] > 25 || inputNotas[2] < 0) {
+          alert("O valor máximo do terceiro bimestre é de 25 pontos e o mínimo 0");
           return true;
-        } else if (i == 3 && inputNotas[3] > 30) {
-          alert("O valor máximo do quarto bimestre é de 30 pontos");
+        } else if (i == 0 && inputNotas[3] > 30 || inputNotas[3] < 0) {
+          alert("O valor máximo do quarto bimestre é de 30 pontos e o mínimo 0");
           return true;
         }
         return false;
@@ -74,18 +92,28 @@ function save() {
     resultNotas.innerHTML = resultados;
   }
 }
-
-function verifMat() {
-  let select = document.querySelector('select').value;
+function verifMat(){
   let erro = false;
+  let matSel = select.value;
 
-  materias.forEach((element) => {
-    if (element.nome == select) {
-      alert("Matéria já adicionada");
-      erro = true;
-    }
-  });
+  materias.forEach(item => {
+    item.nome === matSel ? erro = true: null
+  })
+  erro ? alert("Matéria já adicionada") : null
   return erro;
+}
+
+function verifVazio(div) {
+  let erro = false
+  let inputs = document.querySelectorAll(`.${div} input`)
+  inputs.forEach(input =>{
+    input.value == '' ? erro = true : null
+  })
+
+  erro ? alert("Existe algum campo vazio") : null;
+
+  return erro;
+
 }
 
 function isAprovadoOuReprovado(valor) {
