273e0820a1ba5e1c92bbf30c24a896bade866d8f
Caua Cruz <caualucas34492@gmail.com>
2023-10-18 21:01:39 -0300

    CAMPOS VAZIOS FIM

--- src/pages/atividades/boletim/script-boletim.js
+++ src/pages/atividades/boletim/script-boletim.js
@@ -2,35 +2,20 @@ let materia = { id: 0, nome: "", notas: [], total: 0, printInfo: "" };
 let materias = [];
 let user = { nome: "", instituicao: "", curso: "" };
 
-
-function userSave(){
-
-  if (!verifVazio()) {
-  
-  user.nome = userNome.value;
-  user.instituicao = nomeInstituicao.value;
-  user.curso = nomeCurso.value
-  document.querySelector("#card1").style.display = "none"
-  document.querySelector("#card2").style.display = "flex"
-
-  function verifVazio() {
-    erro = false
-      if (user.nome && user.instituicao && user.curso == "") {
-        alert ("Existe algum campo vazio")
-        return erro;
-      }
-      return true
-      }
+function userSave() {
+  if (!verifVazio("card1")) {
+    user.nome = userNome.value;
+    user.instituicao = nomeInstituicao.value;
+    user.curso = nomeCurso.value;
+    document.querySelector("#card1").style.display = "none";
+    document.querySelector("#card2").style.display = "flex";
+  }
 }
-
-  }  
 function save() {
   let resultados = `<b>BOLETIM DE: ${user.nome.toLocaleUpperCase()}</b><br><b>INSTITUIÇÃO/ESCOLA: ${user.instituicao.toLocaleUpperCase()} <br><b>CURSO: ${user.curso.toLocaleUpperCase()} </b>`;
-  if (!verifMat()) {
+  if (!verifMat() && !verifVazio("card2")) {
     let inputNotas = [];
     let totalNotas = 0;
-    
-    
 
     document.querySelectorAll("div.input-group input").forEach((e) => {
       inputNotas.push(parseFloat(e.value));
@@ -50,45 +35,43 @@ function save() {
                     <th>RESULTADO DE: <b>${select.value.toLocaleUpperCase()}</b></th> <br>
                     
                    
-                        <td><b>1º Bimestre:</b>${
-                          inputNotas[0]
-                        }</td><br>
-                        <td><b>2º Bimestre:</b>${
-                          inputNotas[1]
-                        }</td><br>
-                        <td><b>3º Bimestre:</b>${
-                          inputNotas[2]
-                        }</td><br>
-                        <td><b>4º Bimestre:</b>${
-                          inputNotas[3]
-                        }</td><br>
+                        <td><b>1º Bimestre:</b>${inputNotas[0]}</td><br>
+                        <td><b>2º Bimestre:</b>${inputNotas[1]}</td><br>
+                        <td><b>3º Bimestre:</b>${inputNotas[2]}</td><br>
+                        <td><b>4º Bimestre:</b>${inputNotas[3]}</td><br>
                     </tr> 
                         <td>TOTAL FINAL: <b>${totalNotas}</b></td>
                         <td><br>
-                          STATUS: <b>${isAprovadoOuReprovado(
-                            totalNotas
-                          )}</b>
+                          STATUS: <b>${isAprovadoOuReprovado(totalNotas)}</b>
                         </td>
                     `,
       });
     }
 
     function validNotas() {
-      document.querySelector("#resultNotas").style.display = "block"
-      document.querySelector("#card2").style.width = "1000px"
-      document.querySelector("#card2").style.marginRight = "70px"
+      document.querySelector("#resultNotas").style.display = "block";
+      document.querySelector("#card2").style.width = "1000px";
+      document.querySelector("#card2").style.marginRight = "70px";
       for (let i = 0; i < inputNotas.length; i++) {
-        if (i == 0 && inputNotas[0] > 20 || inputNotas[0] < 0) {
-          alert("O valor máximo do primeiro bimestre é de 20 pontos e o mínimo 0");
+        if ((i == 0 && inputNotas[0] > 20) || inputNotas[0] < 0) {
+          alert(
+            "O valor máximo do primeiro bimestre é de 20 pontos e o mínimo 0"
+          );
           return true;
-        } else if (i == 0 && inputNotas[1] > 25 || inputNotas[1] < 0) {
-          alert("O valor máximo do segundo bimestre é de 25 pontos e o mínimo 0");
+        } else if ((i == 0 && inputNotas[1] > 25) || inputNotas[1] < 0) {
+          alert(
+            "O valor máximo do segundo bimestre é de 25 pontos e o mínimo 0"
+          );
           return true;
-        } else if (i == 0 && inputNotas[2] > 25 || inputNotas[2] < 0) {
-          alert("O valor máximo do terceiro bimestre é de 25 pontos e o mínimo 0");
+        } else if ((i == 0 && inputNotas[2] > 25) || inputNotas[2] < 0) {
+          alert(
+            "O valor máximo do terceiro bimestre é de 25 pontos e o mínimo 0"
+          );
           return true;
-        } else if (i == 0 && inputNotas[3] > 30 || inputNotas[3] < 0) {
-          alert("O valor máximo do quarto bimestre é de 30 pontos e o mínimo 0");
+        } else if ((i == 0 && inputNotas[3] > 30) || inputNotas[3] < 0) {
+          alert(
+            "O valor máximo do quarto bimestre é de 30 pontos e o mínimo 0"
+          );
           return true;
         }
         return false;
@@ -104,30 +87,26 @@ function save() {
     resultNotas.innerHTML = resultados;
   }
 }
-function verifMat(){
+function verifMat() {
   let erro = false;
   let matSel = select.value;
 
-  materias.forEach(item => {
-    item.nome === matSel ? erro = true: null
-  })
-  erro ? alert("Matéria já adicionada") : null
+  materias.forEach((item) => {
+    item.nome === matSel ? (erro = true) : null;
+  });
+  erro ? alert("Matéria já adicionada") : null;
   return erro;
 }
 
-
-// function verifVazio(card1) {
-//   let erro = false
-//   let inputs = document.querySelectorAll(`.${div} input`)
-//   inputs.forEach(input =>{
-//     input.value == "" ? erro = true : null
-//   })
-
-//   erro ? alert("Existe algum campo vazio") : null;
-
-//   return erro;
-
-// }
+function verifVazio(div) {
+  erro = false;
+  let inputs = document.querySelectorAll(`#${div} input`);
+  inputs.forEach((e) => {
+    e.value === "" ? (erro = true) : null;
+  });
+  erro ? alert("Existe algum campo vazio") : null;
+  return erro;
+}
 
 function isAprovadoOuReprovado(valor) {
   if (valor >= 60) {
